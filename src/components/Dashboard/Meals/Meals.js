da50f8493613e8e5e4eb20bbb5b2a65b2ed0de72
kitts <kittsmakokha@gmail.com>
2023-06-09 23:49:30 +0300

    new edits to include login and register

--- src/components/Dashboard/Meals/Meals.js
+++ src/components/Dashboard/Meals/Meals.js
@@ -2,10 +2,12 @@ import { Fragment } from 'react';
 
 import MealsSummary from './MealsSummary';
 import AvailableMeals from './AvailableMeals';
+import Header from '../../layout/Header';
 
 const Meals = () => {
   return (
     <Fragment>
+      <Header/>
       <MealsSummary />
       <AvailableMeals />
     </Fragment>
